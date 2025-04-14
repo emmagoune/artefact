4c3473562130e106ee1c77801f97b372139fb659
Emma Goune <emma.goune@gmail.com>
2025-04-14 12:50:34 +0200

    Fix v0.11

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const imagePairs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg","8.jpg", "9.jpg"]; // List all file names 
+const imagePairs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg","8.jpg", "9.jpg", "10.jpg", "11.jpg"]; // List all file names 
 
 let correctAnswer = "";
 let currentImageName = ""; // To track the current image for file matching
