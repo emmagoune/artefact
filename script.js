124e15aeb400d2d77209f77d140664ec6aeb15e8
Emma Goune <emma.goune@gmail.com>
2025-04-22 13:10:16 +0200

    Fix v1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const imagePairs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg"];
+const imagePairs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg"];
 let remainingPairs = [...imagePairs];
 let correctAnswer = "";
 let currentImageName = "";
